215f1460f327c34440d71c9176cb327ca2b2fa02
허태정 <gjdigj@naver.com>
2018-09-12 12:25:55 +0900

    과제1번 시작

---
+++ assignment4.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n):
+    return 0
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
