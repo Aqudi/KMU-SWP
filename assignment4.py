e6f293db3ba93e1f71c190f4ed1d1e51e9e05559
허태정 <gjdigj@naver.com>
2018-09-12 14:59:49 +0900

    피보나치 수열 함수 만들기

--- assignment4.py
+++ assignment4.py
@@ -1,23 +1,35 @@
 import time
 import random
 
+
 def fibo(n):
     if n <= 1:
         return n
     return fibo(n - 1) + fibo(n - 2)
 
+
 def iterfibo(n):
-    return 0
+    number_list = []
+    if n <= 1:
+        number_list.append(n)
+    else:
+        number_list = [1, 1]
+        for i in range(n):
+                number_list.append(number_list[i] + number_list[i + 1])
+    result = number_list[n-1]
+    return result
+
 
 while True:
     nbr = int(input("Enter a number: "))
+    # nbr = random.randint(30,40)
     if nbr == -1:
         break
     ts = time.time()
     fibonumber = iterfibo(nbr)
     ts = time.time() - ts
-    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    print("IterFibo(%d)= %d, time %.6f" %(nbr, fibonumber, ts))
     ts = time.time()
     fibonumber = fibo(nbr)
     ts = time.time() - ts
-    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    print("Fibo(%d)=     %d, time %.6f" %(nbr, fibonumber, ts))
