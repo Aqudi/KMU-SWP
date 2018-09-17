56f361dff93b50332b04da83f0dc7213fc8a5fcd
허태정 <gjdigj@naver.com>
2018-09-17 22:59:08 +0900

    test코드 삽입 upper와 target의 인덱스값이 같을 때 찾지 못하는 버그를 수정해야함

--- assignment4-1.py
+++ assignment4-1.py
@@ -5,33 +5,41 @@ import random
 def seqsearch(nbrs, target):
     for i in range(0, len(nbrs)):
         if (target == nbrs[i]):
+            print("seqsearch >>", nbrs[i], "  ", i)
             return i
     return -1
 
 
 def recbinsearch(list, low, upper, target):
-    middle = upper+low//2
-    print(middle, low, upper)
-    if target == middle:
+    middle = (upper+low)//2
+    middleNum = list[middle]
+    if upper == low or upper < low:
         return -1
-    elif target > middle:
-        recbinsearch(list, middle, upper, target)
-    elif target < middle:
-        recbinsearch(list, low, middle, target)
-    return -1
+    if target == middleNum:
+        print("{}   {}".format(upper, low))
+        print("binsearch >>", middleNum, "  ", middle)
+
+        return middleNum
+    elif target > middleNum:
+        print("{}   {}".format(upper, low))
+        return recbinsearch(list, middle+1, upper, target)
+    elif target < middleNum:
+        print("{}   {}".format(upper, low))
+        return recbinsearch(list, low, middle-1, target)
+
 
 
 numofnbrs = int(input("Enter a number: "))
 numbers = []
 for i in range(numofnbrs):
-    numbers += [random.randint(0, 999999)]
+    numbers += [random.randint(0, 100)]
 
 numbers = sorted(numbers)
 
 numoftargets = int(input("Enter the number of targets: "))
 targets = []
 for i in range(numoftargets):
-    targets += [random.randint(0, 999999)]
+    targets += [random.randint(0, 100)]
 
 
 ts = time.time()
@@ -39,12 +47,12 @@ ts = time.time()
 # binary search - recursive
 cnt = 0
 for target in targets:
-    idx = recbinsearch(numbers, 0, len(numbers), target)
+    idx = recbinsearch(numbers, 0, len(numbers)-1, target)
     if idx == -1:
         cnt += 1
 ts = time.time() - ts
 print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
-
+print()
 ts = time.time()
 
 # sequential search
@@ -55,3 +63,4 @@ for target in targets:
         cnt += 1
 ts = time.time() - ts
 print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+print(sorted(targets))
