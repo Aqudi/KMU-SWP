81ff292267a0c271c298b39e6bb31ddb42ea8003
허태정 <gjdigj@naver.com>
2018-10-09 20:50:58 +0900

    테스트 1

---
+++ lecture20181004.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+
+class Calculator(QWidget):
+
+    def __init__(self,parent=None):
+
+        super().__init__(parent)
+
+        
+
+
+
---
+++ test.py
@@ -0,0 +1,5 @@
+y = [[{"name":1}, {"score":5}], [{"name":2}, {"score":4}], [{"name":3}, {"score":3}], [{"name":4}, {"score":2}], [{"name":5}, {"score":1}]]
+
+for i in :
+    a = sorted(y, key = lambda x:y["score"]))
+print(a)
