0195cd80f8a32747ef5a445869fe78884699284f
허태정 <gjdigj@naver.com>
2018-10-11 00:50:08 +0900

    Test-Scoredb의 Add, Del, Show, Inc, Find명령어 기능 추가

--- assignment6.py
+++ assignment6.py
@@ -5,6 +5,15 @@ from PyQt5.QtWidgets import (QWidget, QPushButton,
     QComboBox, QTextEdit, QLineEdit)
 from PyQt5.QtCore import Qt
 
+
+class Button(QPushButton):
+
+    def __init__(self, text, callback):
+        super().__init__()
+        self.setText(text)
+        self.clicked.connect(callback)
+
+
 class ScoreDB(QWidget):
 
     def __init__(self):
@@ -21,24 +30,24 @@ class ScoreDB(QWidget):
 
         # 첫번째라인
         lb_name = QLabel("Name", self)
-        le_name = QLineEdit()
+        self.le_name = QLineEdit()
         lb_age = QLabel("Age", self)
-        le_age = QLineEdit()
+        self.le_age = QLineEdit()
         lb_score = QLabel("Score", self)
-        le_score = QLineEdit()
+        self.le_score = QLineEdit()
 
         hbox = QHBoxLayout()
         baseLayout.addLayout(hbox)
         hbox.addWidget(lb_name)
-        hbox.addWidget(le_name)
+        hbox.addWidget(self.le_name)
         hbox.addWidget(lb_age)
-        hbox.addWidget(le_age)
+        hbox.addWidget(self.le_age)
         hbox.addWidget(lb_score)
-        hbox.addWidget(le_score)
+        hbox.addWidget(self.le_score)
 
         #두번쨰라인
         lb_amount = QLabel("Amount", self)
-        le_amount = QLineEdit()
+        self.le_amount = QLineEdit()
         lb_key = QLabel("Key", self)
         self.combo = QComboBox()
         self.combo.addItem("Name")
@@ -49,25 +58,25 @@ class ScoreDB(QWidget):
         baseLayout.addLayout(hbox2)
         hbox2.addStretch(1)
         hbox2.addWidget(lb_amount)
-        hbox2.addWidget(le_amount)
+        hbox2.addWidget(self.le_amount)
         hbox2.addWidget(lb_key)
         hbox2.addWidget(self.combo)
 
         #세번쨰라인
-        btn_add = Button("Add", self.buttonClicked)
-        btn_del = Button("Del", self.buttonClicked)
-        btn_find = Button("Find", self.buttonClicked)
-        btn_inc = Button("Inc", self.buttonClicked)
-        btn_show = Button("Show", self.buttonClicked)
+        self.btn_add = Button("Add", self.buttonClicked)
+        self.btn_del = Button("Del", self.buttonClicked)
+        self.btn_find = Button("Find", self.buttonClicked)
+        self.btn_inc = Button("Inc", self.buttonClicked)
+        self.btn_show = Button("Show", self.buttonClicked)
 
         hbox3 = QHBoxLayout()
         baseLayout.addLayout(hbox3)
         hbox3.addStretch(1)
-        hbox3.addWidget(btn_add)
-        hbox3.addWidget(btn_del)
-        hbox3.addWidget(btn_find)
-        hbox3.addWidget(btn_inc)
-        hbox3.addWidget(btn_show)
+        hbox3.addWidget(self.btn_add)
+        hbox3.addWidget(self.btn_del)
+        hbox3.addWidget(self.btn_find)
+        hbox3.addWidget(self.btn_inc)
+        hbox3.addWidget(self.btn_show)
 
         #네 다섯번쨰라인
         lb_result = QLabel("Result")
@@ -124,16 +133,48 @@ class ScoreDB(QWidget):
         else:
             print("정보가 없습니다.")
 
-    class Button(QPushButton):
 
-        def __init__(self, text, callback):
-            super().__init__()
-            self.setText(text)
-            self.clicked.connect(callback)
 
     def buttonClicked(self):
         button = self.sender()
+        name = self.le_name.text()
+        age = self.le_age.text()
+        score = self.le_score.text()
+        amount = self.le_amount.text()
+        scoredb = self.scoredb
         key = button.text()
+        if key == 'Add':
+            print("add")
+            try:
+                age = int(age)
+                score = int(score)
+                record = {'Name': name, 'Age': age, 'Score': score}
+                self.scoredb += [record]
+            except ValueError:
+                print("pass")
+                pass
+            self.showScoreDB(scoredb)
+
+        elif key == 'Del':
+            for p in scoredb[:]:
+                if p['Name'] == name:
+                    self.scoredb.remove(p)
+            self.showScoreDB(scoredb)
+
+        elif key == 'Show':
+            self.showScoreDB(scoredb)
+
+        elif key == 'Find':
+            temp = []
+            for p in scoredb:
+                if p['Name'] == name:
+                    temp += [p]
+            self.showScoreDB(temp)
+        elif key == 'Inc':
+            for p in scoredb:
+                if p['Name'] == name:
+                    p['Score'] += amount
+            self.showScoreDB(scoredb)
 
 
 if __name__ == '__main__':
