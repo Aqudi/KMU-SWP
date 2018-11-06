fda97a1a01b0ef53c99bd102b407bcb5aea08994
허태정 <gjdigj@naver.com>
2018-11-07 01:28:36 +0900

    Guess클래스

---
+++ hangmanGame/guess.py
@@ -0,0 +1,14 @@
+class Guess:
+
+    def __init__(self, word):
+        # 랜덤으로 뽑은 단어를 설정해서 저장해두기
+        self.word = word
+
+    def display(self):
+        # 결과가 맞는지 틀렸는지 사용자에게 전달
+        pass
+
+
+    def guess(self, character):
+        # 사용자로부터 받은 character가 word안에 있는지 없는지 판단
+        pass
