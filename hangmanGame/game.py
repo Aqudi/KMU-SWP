2b7cd43661abce018ae24e8f19b255f8f1434c09
허태정 <gjdigj@naver.com>
2018-11-07 22:17:18 +0900

    game클래스 게임 종료조건 설정 두개이상의 글자를 입력하면 하나만 입력하라고 안내문 출

--- hangmanGame/game.py
+++ hangmanGame/game.py
@@ -9,11 +9,22 @@ def gameMain():
     hangman = Hangman()
     UI = TextUI(guess, hangman)
 
+    isFinished = False
+
     while hangman.getLife() > 0:
         guessedChar = input("Select a letter: ")
-        if guess.guess(guessedChar) is False:
+        isFinished = guess.guess(guessedChar)
+
+        if len(guessedChar) is not 1:
+            UI.errorPrint("Input just one character")
+        elif isFinished is False:
             hangman.minusLife()
+
         UI.display()
+        if isFinished == True:
+            break
+    UI.endOfGame(isFinished)
+
 
 if __name__ == "__main__":
     gameMain()
