7e5f1389df2d4b534002947baa75d2355a804819
허태정 <gjdigj@naver.com>
2018-11-07 22:18:01 +0900

    UI클래스 endOfGame메소드 게임의 승패에 따라 출력문 다르게 설정

--- hangmanGame/TUI.py
+++ hangmanGame/TUI.py
@@ -6,6 +6,7 @@ class TextUI:
         self.GuessObject = GuessObject
         self.hangmanObject = hangmanObject
         self.display()
+
     def display(self):
         # 결과가 맞는지 틀렸는지 사용자에게 전달
         print("Word: ", self.GuessObject.word)
@@ -13,6 +14,27 @@ class TextUI:
         print(self.hangmanObject.getPicture())
         shownString = " ".join(self.GuessObject.shownList)
         print("Word ({}): {}".format(self.GuessObject.lengthOfWord, shownString))
+        print()
+
+    def endOfGame(self, isFinished):
+        if isFinished == True:
+            print("""
+            =========================================
+            =================Success=================
+            =========================================
+            """)
+        else:
+            print("""
+            =========================================
+            ===================Fail==================
+            =========================================
+            Answer Word = {}
+            """.format(self.GuessObject.word))
+
+    def errorPrint(self, errorMsg):
+        print(errorMsg)
+        print()
+
 
 if __name__ == "__main__":
     c = Guess("abesateda")
