ec1a1437a5a5bff716d37fcdc69a690f869b6947
허태정 <gjdigj@naver.com>
2018-11-07 22:20:10 +0900

    Guess클래스 guessedList에 입력한 글자들을 저장해서 중복입력 방지 종료조건을 충족시키기 위해 정답단어와 입력한 단어가 맞다면 True를 반환 True반환을 확인하는 테스트코드 추

--- hangmanGame/guess.py
+++ hangmanGame/guess.py
@@ -3,7 +3,7 @@ class Guess:
     def __init__(self, word):
         # 랜덤으로 뽑은 단어를 설정해서 저장해두기
         self.word = word
-        self.lengthOfWord = len(self.word) - 1
+        self.lengthOfWord = len(self.word)
         self.shownList = []
         self.guessedList = []
         for i in range(self.lengthOfWord):
@@ -11,14 +11,24 @@ class Guess:
 
     def guess(self, character):
         # 사용자로부터 받은 character가 word안에 있는지 없는지 어디에 있는지 판단
+        self.guessedList.append(character)
+
         if character in self.word:
             for i in range(self.lengthOfWord):
                 if self.word[i] == character:
                     self.shownList[i] = character
-            return " ".join(self.shownList)
+            shownString = "".join(self.shownList)
+            if shownString == self.word:
+                return True
+            else:
+                return shownString
         return False
 
 if __name__ == "__main__":
     c = Guess("abcdaeaf")
     print(c.shownList)
     print(c.guess("a"))
+    d = Guess("abc")
+    d.guess("a")
+    d.guess("b")
+    print(d.guess("c"))
