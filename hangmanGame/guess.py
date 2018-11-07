4d43656f98348f494cff5c76b4db925566e94214
허태정 <gjdigj@naver.com>
2018-11-07 20:57:09 +0900

    guess 메소드의 불필요한 부분들 제거, 클래스테스트코드 추가

--- hangmanGame/guess.py
+++ hangmanGame/guess.py
@@ -9,14 +9,16 @@ class Guess:
         for i in range(self.lengthOfWord):
             self.shownList.append("_")
 
-
     def guess(self, character):
         # 사용자로부터 받은 character가 word안에 있는지 없는지 어디에 있는지 판단
         if character in self.word:
-            indexList = [character]
             for i in range(self.lengthOfWord):
-                if self.shownList[i] == character:
-                    indexList.append(i)
-            self.guessedList.append(indexList)
-            return indexList
+                if self.word[i] == character:
+                    self.shownList[i] = character
+            return " ".join(self.shownList)
         return False
+
+if __name__ == "__main__":
+    c = Guess("abcdaeaf")
+    print(c.shownList)
+    print(c.guess("a"))
