9adf6601826d4182314bf2c5c6a1e4df23a7615b
허태정 <gjdigj@naver.com>
2018-11-07 21:04:48 +0900

    GUess Object를 파라미터로 넘겨서 오류 해결 테스트코드 간단하게 작성

--- hangmanGame/TUI.py
+++ hangmanGame/TUI.py
@@ -1,20 +1,20 @@
 from guess import Guess
 
-class TUI:
-    def __init__(self):
+class TextUI:
+    def __init__(self, GuessObject):
+        self.Object = GuessObject
         self.display()
-        self.shownString = ""
-
-    def display(self, guessedList):
+    def display(self):
         # 결과가 맞는지 틀렸는지 사용자에게 전달
-        for index in guessedList[1:]:
-            del Guess.shownList[index]
-            Guess.shownList.insert(index, guessedList[0])
-        self.shownString.join("")
-        print("Word ({}): {}".format(Guess.lengthOfWord, self.shownString))
+        shownString = " ".join(self.Object.shownList)
+        print("Word ({}): {}".format(self.Object.lengthOfWord, shownString))
 
 if __name__ == "__main__":
-    word = "abest"
-    T = TUI
+    c = Guess("abesateda")
+    print(c.shownList)
+    T = TextUI(c)
+    c.guess("a")
+    T.display()
+
 
 
