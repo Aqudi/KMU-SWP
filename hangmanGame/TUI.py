ee6859a3be4095686c34c96b25847b99d0bfd93f
허태정 <gjdigj@naver.com>
2018-11-07 21:33:56 +0900

    TextUI클래스 hangman 객체를 파라미터로 전달해서 life변수를 사용할 수 있게 만듬 테스트를 위해 단어가 표시되도록 고침 Life에 따라서 그림이 출력되도록 변경

--- hangmanGame/TUI.py
+++ hangmanGame/TUI.py
@@ -1,19 +1,28 @@
 from guess import Guess
+from hangman import Hangman
 
 class TextUI:
-    def __init__(self, GuessObject):
-        self.Object = GuessObject
+    def __init__(self, GuessObject, hangmanObject):
+        self.GuessObject = GuessObject
+        self.hangmanObject = hangmanObject
         self.display()
     def display(self):
         # 결과가 맞는지 틀렸는지 사용자에게 전달
-        shownString = " ".join(self.Object.shownList)
-        print("Word ({}): {}".format(self.Object.lengthOfWord, shownString))
+        print("Word: ", self.GuessObject.word)
+        print("Life: ", self.hangmanObject.getLife())
+        print(self.hangmanObject.getPicture())
+        shownString = " ".join(self.GuessObject.shownList)
+        print("Word ({}): {}".format(self.GuessObject.lengthOfWord, shownString))
 
 if __name__ == "__main__":
     c = Guess("abesateda")
+    h = Hangman()
     print(c.shownList)
-    T = TextUI(c)
+    T = TextUI(c, h)
     c.guess("a")
+    T.display()
+    if c.guess("z") is False:
+        h.minusLife()
     T.display()
 
 
